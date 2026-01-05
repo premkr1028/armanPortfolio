27bd9506ab6a80e8e859ed2ebfa323e04a932aa7
Prem Kumar Balmiki <premkumarbalmiki987@gmail.com>
2026-01-05 17:47:24 +0530

    logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
       <nav className='flex w-screen items-center justify-between p-3  text text-xl font-[500]'>
          <div className='flex w-full items-center justify-between navUnder'>
             <div className="mainNav flex gap-2 items-center">
-               <img className='w-[35px] h-[35px] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.8)] rounded-[20px] ' src="/public/images/armanLogo.jpg" alt="" />
+               <img className='w-[35px] h-[35px] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.8)] rounded-[20px] ' src="/images/armanLogo.jpg" alt="" />
                <Link to='/'>Arman</Link>
 
             </div>
